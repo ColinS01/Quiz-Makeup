1af11c3316533cf0d7a82d62f2f66eccf213d262
ColinSmith14 <106163641+ColinSmith14@users.noreply.github.com>
2022-11-09 08:00:35 -0500

    Final Product

--- src/App.java
+++ src/App.java
@@ -1,5 +1,3 @@
-import java.time.Clock;
-import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalTime;
 import javafx.application.Application;
@@ -7,40 +5,43 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
-public class App {
-
-    
-
+public class App extends Application {
 
     public static void main(String[] args) throws Exception {
         Application.launch(args);
     }
 
-    public void setTime(){
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a");
-        LocalTime time = LocalTime.now();
-        System.out.println(dtf.format(time));
-    }
-
     public void start(Stage stage) throws Exception {
-        StackPane layout = new StackPane();
-        Label CurrentTime = new Label();
-        Button SetTime = new Button();
+        Label CurrentTime = new Label("00:00");
+        Button SetTime = new Button("Set Time");
         GridPane root = new GridPane();
         Scene scene = new Scene(root);
 
+        CurrentTime.setMinSize(10, 10);
+        SetTime.setMinSize(10, 10);
+
+        root.add(CurrentTime, 0, 0);
+        root.add(SetTime, 0, 1);
+
         stage.setScene(scene);
         stage.setTitle("Clock");
-        stage.setWidth(475);
-        stage.setHeight(300);
+        stage.setWidth(200);
+        stage.setHeight(100);
         stage.show();
+        LocalTime time = LocalTime.now();
+
+        SetTime.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a");
+                CurrentTime.setText(dtf.format(time));
+            }
+          });
     }
 }
